aa18ee02b61f5675a644795196fd6f4323b05a5b
zahara12 <ichx.zlm@gmail.com>
2021-09-28 21:49:34 +0700

    second commit

---
+++ LKP5/MyRecyclerView/app/src/main/java/com/example/myrecyclerview/MainActivity.java
@@ -0,0 +1,113 @@
+package com.example.myrecyclerview;
+
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView rvCategory;
+    private ArrayList<President> list = new ArrayList<>();
+    final String STATE_TITLE = "state_string";
+    final String STATE_LIST = "state_list";
+    final String STATE_MODE = "state_mode";
+    int mode;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        rvCategory = findViewById(R.id.rv_category);
+        rvCategory.setHasFixedSize(true);
+
+        list = new ArrayList<>();
+        list.addAll(PresidentData.getListData());
+        showRecyclerList();
+    }
+
+    private void showSelectedPresident(President president) {
+        Toast.makeText(this, "Kamu memilih " + president.getName(), Toast.LENGTH_SHORT).show();
+    }
+
+    private void showRecyclerList() {
+        rvCategory.setLayoutManager(new LinearLayoutManager(this));
+        ListPresidentAdapter listPresidentAdapter = new ListPresidentAdapter(this);
+        listPresidentAdapter.setListPresident(list);
+        rvCategory.setAdapter(listPresidentAdapter);
+
+        ItemClickSupport.addTo(rvCategory).setOnItemClickListener(new ItemClickSupport.OnItemClickListener() {
+            @Override
+            public void onItemClicked(RecyclerView recyclerView, int position, View v) {
+                showSelectedPresident(list.get(position));
+            }
+        });
+    }
+
+    private void showRecyclerGrid() {
+        rvCategory.setLayoutManager(new GridLayoutManager(this, 2));
+        GridPresidentAdapter gridPresidentAdapter = new GridPresidentAdapter(this);
+        gridPresidentAdapter.setListPresident(list);
+        rvCategory.setAdapter(gridPresidentAdapter);
+
+        ItemClickSupport.addTo(rvCategory).setOnItemClickListener(new ItemClickSupport.OnItemClickListener() {
+            @Override
+            public void onItemClicked(RecyclerView recyclerView, int position, View v) {
+                showSelectedPresident(list.get(position));
+            }
+        });
+    }
+
+    private void showRecyclerCardView() {
+        rvCategory.setLayoutManager(new LinearLayoutManager(this));
+        CardViewPresidentAdapter cv = new CardViewPresidentAdapter(this);
+        cv.setListPresident(list);
+        rvCategory.setAdapter(cv);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    private void setActionBarTitle(String title) {
+        getSupportActionBar().setTitle(title);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        setMode(item.getItemId());
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void setMode(int selectedMode) {
+        String title = null;
+        switch (selectedMode) {
+            case R.id.action_list:
+                title = "Mode List";
+                showRecyclerList();
+                break;
+            case R.id.action_grid:
+                title = "Mode Grid";
+                showRecyclerGrid();
+                break;
+            case R.id.action_cardview:
+                title = "Mode CardView";
+                showRecyclerCardView();
+                break;
+        }
+        mode = selectedMode;
+        setActionBarTitle(title);
+    }
+}
